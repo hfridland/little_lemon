51f665db5a648bd4a7772e05c47a5dfc537f1cb7
hfridland <hfridland@shaw.ca>
2023-07-22 19:42:34 -0700

    Hero moved to grid

--- src/components/main/hero/Hero.js
+++ src/components/main/hero/Hero.js
@@ -1,12 +1,23 @@
-import './Hero.css';
-import Content from "./content/Content"
-import Image from "./image/Image"
+import food from './restauranfood.jpg'
+import './Hero.css'
 
 function Hero() {
     return (
-        <section className='hero'>
-            <Content/>
-            <Image/>
+        <section className='hero heroGrid'>
+            <div className='title titleGrid'>Little Lemon</div>
+            <div className='subTitle subTitleGrid'>Chicago</div>
+            <div className='text textGrid'>
+                Lorem ipsum dolor sit amet,
+                consectetur adipiscing elit,
+                sed do eiusmod tempor incididunt
+                ut labore et dolore magna aliqua.
+            </div>
+            <div className="buttonRect buttonRectGrid">
+                <a href="" role="button">Reserve a Table</a>
+            </div>
+            <div className='image imageGrid'>
+            <img src={food} alt='Restaurant food' width={200} />
+            </div>
         </section>
     )
 }
